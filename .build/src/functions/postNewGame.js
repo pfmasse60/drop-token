a731f867348e2adf8008dec6a9ba957659e1a643
Pete Masse <pfmasse60@gmail.com>
2022-01-10 17:27:06 -0700

    fix more changes

--- .build/src/functions/postNewGame.js
+++ .build/src/functions/postNewGame.js
@@ -6,18 +6,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
+exports.handler = exports.baseHandler = void 0;
 const API_Responses_1 = __importDefault(require("../common/API_Responses"));
 const API_Dynamodb_1 = __importDefault(require("../common/API_Dynamodb"));
 const uuid_1 = require("uuid");
+// import core
+const core_1 = __importDefault(require("@middy/core")); // esm Node v14+
+//const middy = require('@middy/core') // commonjs Node v12+
+// import some middlewares
+const http_json_body_parser_1 = __importDefault(require("@middy/http-json-body-parser"));
+const http_error_handler_1 = __importDefault(require("@middy/http-error-handler"));
+const validator_1 = __importDefault(require("@middy/validator"));
 // Serverless invironment variables set inside serverless.yml
 const STATE = process.env.gameState;
 const TABLE_NAME = process.env.gameTableName;
-const handler = async (event) => {
+const baseHandler = async (event) => {
     const gameId = uuid_1.v4();
-    const { players, columns, rows } = JSON.parse(event.body);
-    if (!players || !columns || !rows || players.length != 2)
-        return API_Responses_1.default._400({ 'message': 'Malformed Requst' });
+    const { players, columns, rows } = event.body;
+    console.log(event);
+    // if (!players
+    //     || !columns
+    //     || !rows
+    //     || players.length != 2
+    //     || typeof rows !== 'number'
+    //     || typeof columns !== 'number') 
+    //     return Responses._400({'message': 'Malformed Requst'});
     const [player1, player2] = players;
     await API_Dynamodb_1.default.put(TABLE_NAME, {
         itemType: 'game',
@@ -50,4 +63,33 @@ const handler = async (event) => {
     });
     return API_Responses_1.default._200({ "gameId": gameId });
 };
-exports.handler = handler;
+exports.baseHandler = baseHandler;
+const inputSchema = {
+    type: 'object',
+    properties: {
+        body: {
+            type: 'object',
+            properties: {
+                players: {
+                    type: 'array',
+                    items: {
+                        type: 'string'
+                    },
+                    minItems: 2,
+                    uniqueItems: true
+                },
+                columns: {
+                    type: 'number'
+                },
+                rows: {
+                    type: 'number'
+                }
+            },
+            required: ['players', 'columns', 'rows']
+        }
+    }
+};
+exports.handler = core_1.default(exports.baseHandler)
+    .use(http_json_body_parser_1.default()) // parses the request body when it's a JSON and converts it to an object
+    .use(validator_1.default({ inputSchema })) // validates the input
+    .use(http_error_handler_1.default()); // handles common http errors and returns proper responses
